2dc04e3eaa9e1bd5d455bd2da7c052fe2b5cadfa
Túlio <francatm@outlook.com>
2019-05-14 15:08:18 -0300

    Create and add simple flutter view from java code

--- MyApp/app/src/main/java/com/tulio/myapp/MainActivity.java
+++ MyApp/app/src/main/java/com/tulio/myapp/MainActivity.java
@@ -3,6 +3,11 @@ package com.tulio.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import io.flutter.facade.Flutter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View flutterView = Flutter.createView(MainActivity.this, getLifecycle(), "route1");
+        FrameLayout.LayoutParams frameLayoutParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        addContentView(flutterView, frameLayoutParams);
     }
 }
