57a95fee91779e7186cf27500ba6a2752fb34bee
Túlio <francatm@outlook.com>
2019-05-14 20:55:31 -0300

    Add open flutter view button to android activity

--- MyApp/app/src/main/java/com/tulio/myapp/MainActivity.java
+++ MyApp/app/src/main/java/com/tulio/myapp/MainActivity.java
@@ -5,19 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
 import io.flutter.facade.Flutter;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button openFlutterViewButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        View flutterView = Flutter.createView(MainActivity.this, getLifecycle(), "route1");
-        FrameLayout.LayoutParams frameLayoutParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        addContentView(flutterView, frameLayoutParams);
+        openFlutterViewButton = findViewById(R.id.openFlutterViewButton);
+        openFlutterViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFlutterViewButton.setEnabled(false);
+                View flutterView = Flutter.createView(MainActivity.this, getLifecycle(), "route1");
+                FrameLayout.LayoutParams frameLayoutParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+                frameLayoutParams.topMargin = 200;
+                addContentView(flutterView, frameLayoutParams);
+            }
+        });
     }
 }
